607c65e3f415183bc0a7fa24ae9c475e38e0913e
Thomas Lever <thomaslever508@gmail.com>
2018-06-18 14:11:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function getFirstSelector(selector){
 }
 
 function nestedTarget(){
-  return document.getElementById('app').querySelectorAll('#nested .target')
+  return document.querySelector('#nested.target')
 }
 
 function increaseRankBy(n){
